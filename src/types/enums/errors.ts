fecef732853c96fc742186000468abe27e72e94a
Sararmjalal <sara.jalal.115@gmail.com>
2025-08-25 21:21:09 +0330

    feat: errors enum

---
+++ src/types/enums/errors.ts
@@ -0,0 +1,9 @@
+export const ERRORS = {
+  GET_TODOS: "Failed to fetch todos",
+  GET_TODO: "Failed to fetch todo",
+  ADD_TODO: "Failed to create todo",
+  EDIT_TODO: "Failed to upadte todo",
+  REMOVE_TODO: "Failed to delete todo",
+  TODO_NOT_FOUND: "Todo not found",
+  BAD_REQUEST: "Invalid request parameters"
+}
