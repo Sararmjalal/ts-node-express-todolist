689e41da18be1d93aec9e1e54d9222f74d4343d7
Sararmjalal <sara.jalal.115@gmail.com>
2025-08-26 20:42:35 +0330

    wip: controllers done

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -1,27 +1,46 @@
-import { Request, Response } from "express";
-import { ERRORS } from "../types/enums/errors";
-import { getAllTodos, getSingleTodo } from "../models/todo";
-import { errorResponse, requestPayload, successResponse } from "../lib/utils";
+import { Request, Response, NextFunction } from "express"
+import { ERRORS } from "../types/enums/errors"
+import { MESSAGES } from "../types/enums/messages"
+import { errorResponse, requestPayload, successResponse } from "../lib/utils"
+import { createTodo, deleteTodo, getAllTodos, getSingleTodo, updateTodo } from "../models/todo"
 
 export const getTodos = async (_: Request, res: Response) => {
-  try {
-    const todos = await getAllTodos();
-    res.status(200).json(successResponse(todos));
-  } catch (err) {
-    res.status(500).json(errorResponse(ERRORS.GET_TODOS));
-  }
+  const todos = await getAllTodos()
+  return res.status(200).json(successResponse(todos, MESSAGES.GET_TODOS))
 }
 
 export const getTodo = async (req: Request, res: Response) => {
-  try {
-    const { id } = requestPayload<{ id: string }>(req)
-    if (!id)
-      res.status(400).json(errorResponse(ERRORS.BAD_REQUEST))
-    const thisTodo = getSingleTodo(id)
-    if (!thisTodo)
-      res.status(404).json(errorResponse(ERRORS.TODO_NOT_FOUND))
-    res.status(200).json(successResponse(thisTodo));
-  } catch (err) {
-    res.status(500).json(errorResponse(ERRORS.GET_TODO));
-  }
+  const { id } = requestPayload<{ id: string }>(req)
+  if (!id) return res.status(400).json(errorResponse(ERRORS.BAD_REQUEST))
+
+  const thisTodo = await getSingleTodo(id)
+  if (!thisTodo) return res.status(404).json(errorResponse(ERRORS.TODO_NOT_FOUND))
+
+  return res.status(200).json(successResponse(thisTodo, MESSAGES.GET_TODO))
+}
+
+export const addTodo = async (req: Request, res: Response) => {
+  const { text } = requestPayload<{ text: string }>(req)
+  if (!text) return res.status(400).json(errorResponse(ERRORS.BAD_REQUEST))
+
+  const thisTodo = await createTodo(text)
+  return res.status(200).json(successResponse(thisTodo, MESSAGES.ADD_TODO))
+}
+
+export const editTodo = async (req: Request, res: Response) => {
+  const { id, text } = requestPayload<{ id: string; text: string }>(req)
+  if (!id || !text) return res.status(400).json(errorResponse(ERRORS.BAD_REQUEST))
+
+  const thisTodo = await updateTodo(id, text)
+  if (!thisTodo) return res.status(404).json(errorResponse(ERRORS.TODO_NOT_FOUND))
+
+  return res.status(200).json(successResponse(thisTodo, MESSAGES.EDIT_TODO))
+}
+
+export const removeTodo = async (req: Request, res: Response) => {
+  const { id } = requestPayload<{ id: string }>(req)
+  if (!id) return res.status(400).json(errorResponse(ERRORS.BAD_REQUEST))
+
+  await deleteTodo(id)
+  return res.status(200).json(successResponse(undefined, MESSAGES.REMOVE_TODO))
 }
