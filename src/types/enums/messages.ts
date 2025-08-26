0c25824bf0578d25eb069c0bc0d3adbcab6b7e8b
Sararmjalal <sara.jalal.115@gmail.com>
2025-08-26 20:41:37 +0330

    feat: messages enum

---
+++ src/types/enums/messages.ts
@@ -0,0 +1,7 @@
+export const MESSAGES = {
+  GET_TODOS: "OK",
+  GET_TODO: "OK",
+  ADD_TODO: "Todo added successfully",
+  EDIT_TODO: "Todo updated successfully",
+  REMOVE_TODO: "Todo removed successfully",
+}
